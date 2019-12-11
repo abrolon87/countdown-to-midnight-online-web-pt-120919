b604eb5d00fe1a6717ad9fb049d2a895d9afc09e
Amanda Rolon <abrolon87@gmail.com>
2019-12-11 16:12:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown
-  counter = 10
-  while counter > 0
-    puts "#{counter} SECOND(S)!"
-    counter +=1
+def countdown(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
   end
   "HAPPY NEW YEAR!"
 end
